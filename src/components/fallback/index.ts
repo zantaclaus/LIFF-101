6452592f1b727ba5539aea68de398e291871cc88
Zantaclaus <tarzuto@gmail.com>
2024-03-10 15:56:16 +0700

    :sparkles: Implement Liff Loading component

---
+++ src/components/fallback/index.ts
@@ -0,0 +1,1 @@
+export { default as LiffLoading } from "./liff-loading";
---
+++ src/components/fallback/liff-loading.tsx
@@ -0,0 +1,9 @@
+const LiffLoading = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <p>LIFF Loading...</p>
+    </div>
+  );
+};
+
+export default LiffLoading;
