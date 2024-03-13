073c5369a4283a054c25763526a9c2f8ee69ffb7
Zantaclaus <tarzuto@gmail.com>
2024-03-13 22:52:25 +0700

    :sparkles: Render Profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Profile } from "@/components/users";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <Profile />
+    </div>
+  );
 }
